27fa5e9ccd97ba41151decb61cfeb06db35992d0
Tobias Leich <email@froggs.de>
2011-07-17 21:01:49 +0200

    all Xlibs dynamically linked

--- staticperl_cmd.sh
+++ staticperl_cmd.sh
@@ -5,7 +5,7 @@
 --add share/background.png --add share/red.png --add share/green.png --add share/yellow.png --add share/pink.png --add share/blue.png \
 --staticlib aa --staticlib artsc --staticlib asound --staticlib asyncns --staticlib audio --staticlib direct --staticlib directfb \
 --staticlib esd --staticlib fusion --staticlib gpm --staticlib ncurses --staticlib resolv --staticlib rt --staticlib slang \
---staticlib vga --staticlib x86 --staticlib xcb --staticlib xcb-xlib --staticlib Xau --staticlib Xdmcp --staticlib Xi \
+--staticlib vga --staticlib x86 \
 --staticlib SDL --staticlib SDLmain \
 --staticlib z --staticlib png --staticlib tiff --staticlib jpeg --staticlib SDL_image \
 --staticlib ogg --staticlib vorbis --staticlib vorbisfile --staticlib smpeg --staticlib mikmod --staticlib pulse --staticlib pulse-simple --staticlib SDL_mixer \
@@ -13,3 +13,4 @@
 --staticlib SDL_gfx \
 --staticlib m --staticlib crypt --staticlib stdc++
 
+#--staticlib xcb --staticlib xcb-xlib --staticlib Xau --staticlib Xdmcp --staticlib Xi --staticlib X11 \
